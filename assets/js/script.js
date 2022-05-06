bf2d4a043e87f5fc155012193f23ea10f114aa37
CarolinaOrellana <caryorellanam@gmail.com>
2022-05-05 23:38:29 -0400

    navbar

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+//Navbar background
+var nav = document.querySelector('nav');
+
+window.addEventListener('scroll', function(){
+    if(window.pageYOffset >100){
+        nav.classList.add('bg-info', 'shadow');
+    }else{
+        nav.classList.remove('bg-info', 'shadow');
+    }
+})
